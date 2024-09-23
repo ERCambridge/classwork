aa9af167dfa0ef9fa8fefee60bee7de9056ead1b
Ethan Cambridge <ethancambridge@hotmail.com>
2024-09-23 18:34:16 -0400

    adding files

--- Unit-5-Unit-Testing/Day-3-File-IO/Day-3-File-IO/Program.cs
+++ Unit-5-Unit-Testing/Day-3-File-IO/Day-3-File-IO/Program.cs
@@ -9,6 +9,63 @@ namespace Day_3_File_IO
         {
             _myFuncs.WriteSeparatorLine("Welcome to a File I/O Example");
 
+            //Display the current Execution folder (folder the app is running from)
+            //The environment object gives you access to information about the current execution environment 
+            Console.WriteLine($"Current execution folder is: {Environment.CurrentDirectory}");
+
+            //Display all the lines in the file called: numbers.txt in files directory/folder
+            //Define a StreamReader for the file path containing the file
+            //files path is 3 levels up from the execution path
+            //StreamReader numberReader = new StreamReader("../../../files/numbers.txt");
+            //Define a StreamWriter object to hold the output from the program in a file
+            StreamWriter outputWriter = new StreamWriter("../../../silence.output");
+
+            //Define a running total outside the loop
+            int runningTotal = 0;
+
+            try
+            {
+                using (StreamReader numberReader = new StreamReader("../../../files/numbers.txt"))
+                    //Loop until all the lines in the file have been processed (not the EndOfStream)
+                    while (!numberReader.EndOfStream) //.EndOfStream will indicate when file is empty (no more data)
+                    {
+                        //Get a line from the file
+                        string currentNum = numberReader.ReadLine();
+
+                        //Display and sum the numbers
+                        //Split line into values
+                        string[] theValues = currentNum.Split(",");
+
+                        int sum = 0;
+
+                        //Loop through the values
+                        for (int i = 0; i < theValues.Length; i++)
+                        {
+                            //Trim any leading or trailing spaces from the value
+                            string aValue = theValues[i].Trim();
+                            //Convert a value from string to int
+                            int intValue = int.Parse(aValue);
+                            //Add the int value to the sum
+                            sum += intValue;
+                            //Display the value 
+                            outputWriter.WriteLine($"Value #{i}: {aValue}");
+                        }
+                        //After we processed all the numbers
+                        outputWriter.WriteLine($"The sum is: {sum}");
+                        runningTotal += sum;    
+
+                        //Display the line
+                        //Console.WriteLine(currentNum);               
+                    }
+            }
+            catch (Exception ex)
+            {
+                outputWriter.WriteLine(ex.Message );
+
+            }
+            outputWriter.WriteLine($"The running subtotal is: {runningTotal}");
+            //numberReader.Close();
+            outputWriter.Close();
 
             _myFuncs.PauseProgram();
             _myFuncs.WriteSeparatorLine("Thanks for visiting a File I/O Example");
---
+++ Unit-5-Unit-Testing/My-Logging-Example/Program.cs
@@ -0,0 +1,56 @@
+﻿using GeneralPurposeFunctions;
+
+namespace My_Logging_Example
+{
+    internal class Program
+    {
+        static CommonlyUsedFunctions myFuncs = new CommonlyUsedFunctions();
+        static StreamWriter logFile = new StreamWriter("../../../program.log", true);
+
+        static string inputFilePath = "../../../data/IMDB-Top-25-SciFi-Movies.txt";
+        static void Main(string[] args)
+        {
+            LogEvent("Program started");
+           //Read and display all the data in the IMDB-Top-25-SciFi-Movies file
+
+           //Define a Stream Reader for the file
+           StreamReader fileReader = new StreamReader(inputFilePath);
+
+            //Define a List to hold each line from the file
+            List<string> sciFiMovies = new List<string>();
+
+            while (!fileReader.EndOfStream) 
+            {
+               sciFiMovies.Add(fileReader.ReadLine()); 
+            }
+
+            do
+            {
+                Console.WriteLine("Enter a word in the title of the movie you're looking for...");
+
+                string userInput = Console.ReadLine();
+
+                foreach (string aLine in sciFiMovies)
+                {
+                    if (aLine.ToLower().Contains(userInput.ToLower()))
+                    {
+                        Console.WriteLine(aLine);
+                    }
+                }
+            } while (myFuncs.moreInput());
+
+
+            fileReader.Close();
+            LogEvent("Program ended"); 
+            logFile.Close();
+        }
+
+        public static void LogEvent(string messageToLog) 
+        {
+            DateTime now = DateTime.Now;
+            string formattedTime = now.ToString("yyyy-MM-dd-HH:mm:ss");
+            logFile.WriteLine($"{formattedTime} - {messageToLog}");
+            logFile.Flush();
+        }
+    }
+}
