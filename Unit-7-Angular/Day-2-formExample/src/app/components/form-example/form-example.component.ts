c058ed5720fd73296e67e2cd23e39c595cb874da
Ethan Cambridge <ethancambridge@hotmail.com>
2024-10-31 18:30:26 -0400

    adding files

--- Unit-7-Angular/Day-2-formExample/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-2-formExample/src/app/components/form-example/form-example.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { throwError } from 'rxjs';
@@ -5,15 +6,55 @@ import { throwError } from 'rxjs';
 @Component({
   selector: 'form-example',  // Name used in html to reference the component <form-example>
   standalone: true,
-  imports: [FormsModule],  // Tell Angular we want Forms support for this code
+  imports: [FormsModule, CommonModule],  // Tell Angular we want Forms support for this code
   templateUrl: './form-example.component.html',
   styleUrl: './form-example.component.css'
 })
 // Allow access to stuff defined this component from outside the component
 export class FormExampleComponent {
-  buttonClicked = false
+  firstName = ""
+  lastName = ""
+  birthDate = ""
+  foodPref = ""
+  email = ""
+  emailPref = ""
+
+  checkboxes: any[] = [
+    {name: 'American Football', value: "Football"},
+    {name: 'European Football', value: "Soccer"},
+    {name: 'Baseball', value: "Baseball"},
+    {name: 'Basketball', value: "Basketball"},
+    {name: 'Hockey', value: "Hockey"},
+    {name: 'Cricket', value: "Cricket"},
+    {name: 'Other Sport', value: "Other"},
+    {name: 'None', value: "None"}
+  ]
+
   onSubmit() { // Handle the Submit button click on the  form
-    this.buttonClicked = true
-    console.log(`OK.... You Clicked Submit`)
+    console.log(`First Name ${this.firstName}`)
+    console.log(`Last Name ${this.lastName}`)
+    console.log(`Birth Date ${this.birthDate}`)
+    console.log(`Food Preference ${this.foodPref}`)
+    console.log(`Email ${this.email}`)
+    console.log(`Ok to email ${this.emailPref}`)
+    console.log(`Sports they like: `)
+    // console.table(this.checkBoxResult)
+    this.checkBoxResult.forEach(element => {
+      console.log(element.name)
+      
+    });
+  }
+  
+  get checkBoxResult(){  
+    return this.checkboxes.filter(aCheckbox => aCheckbox.isChecked)
+  }
+
+  checkAllOptions(){
+    if(this.checkboxes.every((aCheckbbox) => aCheckbbox.isChecked == true)){
+      this.checkboxes.forEach(anElement => {anElement.isChecked = false});
+    }
+    else{
+      this.checkboxes.forEach(anElement => {anElement.isChecked = true});
+    }
   }
 }
