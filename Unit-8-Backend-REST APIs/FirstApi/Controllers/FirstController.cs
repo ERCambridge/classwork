9f1907bcefa6bed9dd7fa338f7857a5d2a2be3c5
Ethan Cambridge <ethancambridge@hotmail.com>
2024-12-02 18:33:00 -0500

    adding files

---
+++ Unit-8-Backend-REST APIs/FirstApi/Controllers/FirstController.cs
@@ -0,0 +1,47 @@
+﻿using FirstApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+
+    public class FirstController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult actionResult()
+        {
+            return Ok("Welecome to Ethan's Controller");
+        }
+
+        [HttpGet("/gambler")]
+        public IActionResult getGamblers() 
+        {
+            GamblerDao newData = new GamblerDao();
+            return Ok(newData.getGamblers());
+        }
+
+        [HttpGet("/gambler/{id}")]
+        public IActionResult getGamblerByID(int id)
+        {
+            GamblerDao newData = new GamblerDao();
+            return Ok(newData.getGamblerByID(id));
+        }
+
+        [HttpGet("/findgambler")]
+        public IActionResult getGamblerByName(string name)
+        {
+            GamblerDao newData = new GamblerDao();
+            return Ok(newData.getGamblerByName(name));
+        }
+
+        [HttpPost("/gambler")]
+         
+        public IActionResult addGambler(Gambler newGambler)
+        {
+            GamblerDao newData = new GamblerDao();
+            return Ok(newData.addGambler(newGambler));
+        }
+    }
+}
---
+++ Unit-8-Backend-REST APIs/FirstApi/Model/Gambler.cs
@@ -0,0 +1,47 @@
+﻿namespace FirstApi.Model
+{
+    public class Gambler
+    {
+        // Data members
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Salary { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        // 0-arg constructor may be used by C# when needed
+        public Gambler() { }
+
+        // 5-arg constructor
+        public Gambler(int id, string name, string address, double salary, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Salary = salary;
+            BirthDate = birthDate;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Gambler gambler &&
+                   Id == gambler.Id &&
+                   Name == gambler.Name &&
+                   Address == gambler.Address &&
+                   Salary == gambler.Salary &&
+                   BirthDate == gambler.BirthDate;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Address, Salary, BirthDate);
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
+
