f5028e226e9a19fda5b79a02154bab142c430323
Ethan Cambridge <ethancambridge@hotmail.com>
2024-12-11 21:54:52 -0500

    adding files

---
+++ Unit-8-Backend-REST-APIs/ConsumeAPIsC#/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using ConsumeAPIsC_.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using moviesAPI.Models;
+
+namespace ConsumeAPIsC_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieService _service;
+
+        public MovieController(MovieService movieService)
+        {
+            _service = movieService;
+        }
+        [HttpGet("movies")]
+        public Task<List<Movie>> GetMovies()
+        {
+            return _service.GetAllMovies();
+        }
+
+        [HttpPost("movies/create")]
+        public Task<Movie> CreateMovie(Movie newMovie)
+        {
+            return _service.AddMovie(newMovie);
+        }
+
+        [HttpDelete("movies/delete/{id}")]
+        public Task<Movie> DeleteMovie(int id)
+        {
+            return _service.RemoveMovie(id);
+        }
+
+        [HttpPut("movies/update")]
+        public Task<Movie> PutMovie(Movie movie)
+        {
+            return _service.UpdateMovie(movie);
+        }
+    }
+}
---
+++ Unit-8-Backend-REST-APIs/ConsumeAPIsC#/Services/MovieService.cs
@@ -0,0 +1,64 @@
+﻿using moviesAPI.Models;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace ConsumeAPIsC_.Services
+{
+    public class MovieService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MovieService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7223/api/MovieApi/");
+        }
+        public async Task<List<Movie>> GetAllMovies()
+        {
+            HttpResponseMessage theResponse = await _httpClient.GetAsync("movies");
+
+            //Movie[] movies = await _httpClient.GetFromJsonAsync<Movie[]>("movies");
+
+            string dataReturned = await theResponse.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<List<Movie>>(dataReturned);
+        }
+
+        public async Task<Movie> AddMovie(Movie newMovie)
+        {
+            string jsonForObject = JsonSerializer.Serialize(newMovie);
+
+            StringContent theData = new StringContent(jsonForObject, System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage thePostResponse = await _httpClient.PostAsync("movies/create", theData);
+
+            String dataReturnedPost = await thePostResponse.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<Movie>(dataReturnedPost);
+
+        }
+
+        public async Task<Movie> RemoveMovie(int id)
+        {
+            HttpResponseMessage theDeleteResponse = await _httpClient.DeleteAsync($"movies/delete/{id}");
+
+            String dataReturnedPost = await theDeleteResponse.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<Movie>(dataReturnedPost);
+
+        }
+
+        public async Task<Movie> UpdateMovie(Movie movie)
+        {
+            string jsonForObject = JsonSerializer.Serialize(movie);
+
+            StringContent theData = new StringContent(jsonForObject, System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage thePutResponse = await _httpClient.PutAsync("movies/update", theData);
+
+            String dataReturnedPut = await thePutResponse.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<Movie>(dataReturnedPut);
+        }
+    }
+}
